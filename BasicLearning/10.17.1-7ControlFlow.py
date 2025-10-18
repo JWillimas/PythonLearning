5caa398174ffa1c243257789457d6b943d1fce16
JWillimas <1989687823@qq.com>
2025-10-18 11:29:47 +0800

    25.10.18

--- BasicLearning/10.17.1-7ControlFlow.py
+++ BasicLearning/10.17.1-7ControlFlow.py
@@ -5,8 +5,10 @@
 
 # 2-Conditional Statements
 
-temp = input("temp =")
-temp = int(temp)
+# temp = input("temp =")
+# temp = int(temp)
+
+
 # if temp > 22:
 #     print("sweating!!!")
 # elif temp > 20:
@@ -14,6 +16,48 @@ temp = int(temp)
 # else:
 #     print("cold!!!")
 
-# ternary operator
-message = "Sweating!!!"if temp > 22 else "Nice weather" if temp > 20 else "cold!!!"
-print(message)
+# 3-ternary operator:
+
+# message = "Sweating!!!"if temp > 22 else "Nice weather" if temp > 20 else "cold!!!"
+# print(message)
+
+# 4-logical operators : and,or,not
+# Credit_input = input("Good_Credit=Y/N:")
+# if Credit_input == "Y":
+#     good_credit = True
+# elif Credit_input == "N":
+#     good_credit = False
+# else:
+#     print("Invalid input please enter Y or N")
+
+# High_income_input = input("High_Income=Y/N:")
+# if High_income_input == "Y":
+#     high_income = True
+# elif High_income_input == "N":
+#     high_income = False
+# else:
+#     print("Invalid inout please enter Y or N  ")
+
+# Student_identity_input = input("Student__identity=Y/N:")
+# if Student_identity_input == "Y":
+#     student_identity = True
+# elif Student_identity_input == "N":
+#     student_identity = False
+# else:
+#     print("Invalid inout please enter Y or N  ")
+
+# if (good_credit and high_income) and not student_identity:
+#     print("Eligible for loan")
+# else:
+#     print("Not eligible for loan")
+
+# 5-Short-circuiting Evaluation
+
+# 6-chaining Comparison Operators
+
+# age = input("age=")
+# age = int(age)
+# if 18 <= age < 65:
+#     print("Eligible for work")
+# else:
+#     print("Not eligible for work")
