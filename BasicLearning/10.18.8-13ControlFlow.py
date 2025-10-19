6b80a38c191913bf16ee141e9fdd24cb83180c2f
JWillimas <1989687823@qq.com>
2025-10-19 10:45:21 +0800

    25.10.19

---
+++ BasicLearning/10.18.8-13ControlFlow.py
@@ -0,0 +1,48 @@
+# 8-For Loop
+# for i in range(5):
+#     print("Iteration:", i)
+
+# print("\n")
+
+# for i in range(1, 5):
+#     print("Iteration:", i)
+
+# print("\n")
+
+# for i in range(1, 10, 2):
+#     print("Iteration:", i)
+
+# question:How do we using a Function in Loop?
+
+# 9-for...else
+
+# Successful = False
+# for i in range(5):
+#     print("Iteration:", i)
+#     if Successful:
+#         print("Successful")
+#         break
+
+# else:
+#     (print("Iteration 5 times and failed"))
+
+# print("\n")
+
+# 10-nested Loops
+
+# for x in range(3):
+#     for y in range(2):
+#         print(f"({x},{y})")
+
+# 11-Iterables(string , list , range)
+# print(type("hellow"))
+# print(type(range(5)))
+
+# for i in range(5):
+#     print(i)
+
+# for string in "String":
+#     print(string)
+
+# for list in [1, 3, 5, 7, 9]:
+#     print(list)
