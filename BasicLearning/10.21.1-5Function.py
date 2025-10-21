96ce606671d1b6007818b8fad303f7114b0fe50c
JWillimas <1989687823@qq.com>
2025-10-21 23:21:02 +0800

    25.10.21

--- BasicLearning/10.21.1-5Function.py
+++ BasicLearning/10.21.1-5Function.py
@@ -2,3 +2,50 @@
 # in This section you will learn to write your own functions
 
 # 2-Arugment
+# def greet(first_name, Last_name):
+#     print(f"Hellow{first_name}{Last_name}")
+#     print("Welcome Aborad\n")
+
+
+# greet("John", "Willimas")
+# greet("Mary", "Jeans")
+
+
+# 3-Types of Functions
+
+# 1-perform a task
+# 2-Return a value
+
+# def greet(first_name, Last_name):
+#     return f"{first_name}{Last_name}\n"
+
+
+# message = greet("John", "Willimas")
+# context = (f"Hi There,{message}")
+# print(context)
+
+# def greet(name):
+#     print(f"Hellow {name}")
+#     return "..."
+
+
+# print(greet("John"))  # How the print function Read and Work?
+
+# 4-Keyword Argument
+# def increment(number, by):
+#     return number+by
+
+
+# print(increment(number=2, by=1))  # Keyword Argument
+# Use to clarify the meaning of the argument
+
+# 5-Default Argument
+# All the optianl parameters
+# should behind of the required parameters
+
+# def increment(number, by=1,):  # number->Required Argument
+#     # by->(Optional)Default Argument
+#     return number+by
+
+
+# print(increment(2, 5))
