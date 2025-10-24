e26a57cb33d62551422d4f67a2293844459383cf
JWillimas <1989687823@qq.com>
2025-10-24 10:18:18 +0800

    25.10.24

---
+++ BasicLearning/10.23DataStructure.py
@@ -0,0 +1,25 @@
+# 1-list
+# list can store num,letter,Matrix...
+# Letter = ["a", "b", "c"]
+# print(f"{Letter}\n")
+
+# Matrix = [[1, 2], [2, 3], [3, 4]]
+# print(f"{Matrix}\n")
+
+# Zeros = [0]*5
+# print(f"{Zeros}\n")
+
+# Combined = Zeros+Matrix
+# print(f"{Combined}\n")
+
+# # Using of rang and list function
+# # Rang is iterable
+# Number = list(range(1, 20))
+# # range is start defaults to 10 ,and stop is omitted
+# print(f"{Number}\n")
+
+# char = list("hellow world")
+# print(f"{char}\n")
+# print(len(char))
+
+# 2-
