63bed6e2e89e3dce42e3947347a27ae18fe934ec
JWillimas <1989687823@qq.com>
2025-10-25 17:21:00 +0800

    25.10.25

--- BasicLearning/10.23DataStructure.py
+++ BasicLearning/10.23DataStructure.py
@@ -22,4 +22,31 @@
 # print(f"{char}\n")
 # print(len(char))
 
+
 # 2-Accessing Items
+# letters = list("abcd")
+# Letters = ["A", "B", "C", "D"]
+
+# Numbers = list(range(20))
+
+
+# letters[0] = "A"
+
+# print(Numbers[::-1])
+# this will return all the items in the original list
+# but in reverse order.
+
+# 3-Unpacking Lists
+
+# numbers = [1, 2, 3, 4, 4, 4]
+# fir, sec, *others = numbers
+# unpacking-unpack the numbers to the
+# left side of silent operator,use asterisk there
+# packing all the other items into a seperate list
+# items--->>>pack--->>to list
+# like :def multiply(*items)
+#               items(1,2,3,4)
+
+# items<<-----unpack<<----list
+
+# 4-Loop Over List
