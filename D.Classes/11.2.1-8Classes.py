5bcb3408137e42fd3c83633076d04c49cbd37802
JWillimas <1989687823@qq.com>
2025-11-03 23:46:50 +0800

    25.11.3

--- D.Classes/11.2.1-8Classes.py
+++ D.Classes/11.2.1-8Classes.py
@@ -131,3 +131,31 @@
 
 # point = Point(1.1, 1.1)  # factory method
 # print(str(point))
+
+
+# 7-Comparing Objects
+# class Point:
+#     defualt_color = "red"
+
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def __str__(self):
+#         return f"({self.x},{self.y}) "
+
+#     def __eq__(self, other):    # ==
+#         return self.x == other.x and self.y == other.y
+
+#     def __lt__(self, other):
+#         return self.x < other.x
+
+#     def draw(self):
+#         print(f"Point({self.x},{self.y})")
+
+
+# point = Point(1, 1)
+# other = Point(2, 1)
+# print(point < other)
+
+# 8-Arithmetic Operation
---
+++ D.Classes/11.2.9Classes.py
@@ -0,0 +1,1 @@
+# 9-Creating Custom Containers
