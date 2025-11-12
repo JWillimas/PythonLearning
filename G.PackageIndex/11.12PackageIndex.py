dcf8071ceac2ee798e1be6e5682516b16ff15618
JWillimas <1989687823@qq.com>
2025-11-12 23:39:27 +0800

    25.11.12

---
+++ G.PackageIndex/11.12PackageIndex.py
@@ -0,0 +1,25 @@
+#---------------------------------------
+#1-install Request
+#put it in shell:
+#python -m pip install requests
+
+
+#---------------------------------------
+#2-Virtual Enviorment
+#Promble :we have another project in rhat project 
+#we only use the earlier version of the package
+
+#put it in shell:
+#python -m pip install virtualenv
+
+#Create a Virtual enviorment
+#:cd D:\VScodeFile\PythonLearning
+#python -m venv venv
+
+#And go to 
+# D:\VScodeFile\PythonLearning\G.Packagning\G.PackageIndex\.venv\Scripts\Activate.ps1
+#to anctivite the venv
+import requests
+print(requests.get("http://google.com"))
+
+#---------------------------------------
